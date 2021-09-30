f3971d28ef0004f9b63df6db7c65b6a5d80f2233
divyanshchandna <divyanshchandna@gmail.com>
2021-09-30 23:08:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function calculateProfitOrLoss(){
     outputDiv.style.visibility = "visible";
     if(price.value && quantity.value && presentValue.value){
         if(Number(price.value) >0 && Number(quantity.value) > 0 && Number(presentValue.value) > 0){
-            var profit = (Number(presentValue.value) - Number(price.value));
+            var profit = (Number(presentValue.value) - Number(price.value)) * Number(quantity.value);
             if(profit > 0){
-                var profitPercentage = (profit/Number(price.value)) * 100;
+                var profitPercentage = (profit/(Number(price.value)*Number(quantity.value))) * 100;
                 outputDiv.style.color = "#22C55E";
                 outputDiv.innerText = "Yaaay! Profit is " + profit + ". Profit Percentage is " + profitPercentage.toFixed(2) + "%";
             }
             else if(profit < 0){
-                var lossPercentage = (-profit/Number(price.value)) * 100;
+                var lossPercentage = (-profit/(Number(price.value)*Number(quantity.value))) * 100;
                 outputDiv.style.color = "#DC2626";
                 outputDiv.innerText = "Oops! Loss is " + profit*-1 + ". Loss Percentage is " + lossPercentage.toFixed(2) + "%";
             }
